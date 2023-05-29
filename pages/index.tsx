18cae563d63b30a2ddb3787b17f0e803c9be958d
Helina0610 <hongjuy0610@gmail.com>
2023-05-29 18:26:41 +0900

    feat : getDatabase from notion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { getDatabaseItems } from '@/cms/notionClient';
+import { GetStaticProps } from 'next';
 import React from 'react'
 
 const index = () => {
@@ -6,4 +8,13 @@ const index = () => {
   )
 }
 
-export default index
+export default index;
+
+export const getStaticProps : GetStaticProps = async () => {
+  if(!process.env.DATABASE_ID) throw new Error('DATABASE_ID is not undifibed');
+  const databaseItems = await getDatabaseItems(process.env.DATABASE_ID)
+  // console.log("databaseItems :>>",databaseItems)
+  return{
+    props : {}
+  }
+}
