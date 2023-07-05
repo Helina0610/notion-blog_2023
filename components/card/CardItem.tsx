bb87df66e55eb8c81d1bbbe43cb5089f8d11cb6d
Helina0610 <hongjuy0610@gmail.com>
2023-07-05 18:25:36 +0900

    feat : tagItem 추가

--- components/card/CardItem.tsx
+++ components/card/CardItem.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { describe } from 'node:test';
 import IconRenderer from './IconRenderer';
+import TageList from './tag/TageList';
 
 interface CardItemProps {
 	cardItem : ParseDatabaseItemsType
@@ -13,13 +14,13 @@ interface CardItemProps {
 export const CardItem = ({cardItem} : CardItemProps) => {
 	const {cover,decsription,icon,id,published, tags,title} = cardItem
   return (
-    <li className='rounded-3xl overflow-hidden shadow-lg group'>
+    <li className='rounded-3xl overflow-hidden shadow-lg group flex  flex-col'>
         <Link href={`blog/${id}`}>
-					<a>
+					<a className=' flex-grow'>
 						<div className='relative aspect-[1.3/1]'>
 							<Image src={cover} alt={title} layout='fill' className=' group-hover:scale-105 translate-transform'/>
 						</div>
-						<div className='p-6 flext flex-col gap-5 '>
+						<div className='p-6 flext flex-col gap-4 '>
 							<h4 className='font-bold text-2xl group-hover:text-blue-600 transition-colors flex flex-row items-center gap-1'>
 								<IconRenderer icon={icon}/>
 								{title}
@@ -29,6 +30,7 @@ export const CardItem = ({cardItem} : CardItemProps) => {
 						</div>
 					</a>
 				</Link>
+        { tags.length > 0 ? <TageList tags={tags}/> : null}
     </li>
   )
 }
